652de70eebe9d05725000c71d85cfa11e269d114
zl3311 <li3311@purdue.edu>
2021-08-28 21:39:49 -0400

    some more changes

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,7 +1,8 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-from sklearn.ensemble.fore
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
-# Optional: implement hyperparameter tuning.
+
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -17,8 +18,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +59,16 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
+
+
+def save_to_file(instance, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(instance, f)
+    return
+
+
+def load_from_file(filename):
+    with open(filename, "rb") as f:
+        model = pickle.load(f)
+    return model
